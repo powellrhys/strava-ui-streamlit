3a5ce9a93b3a9602dad8fe804957340e6761ddf0
rhys powell <rhys.powell2@btinternet.com>
2024-10-04 16:32:24 +0100

    comments added to class

--- functions/variables.py
+++ functions/variables.py
@@ -1,6 +1,12 @@
 import datetime as dt
 
 class Variables:
+    '''
+    Input: None
+    Output: None
+    Class to hold all codebase constants
+    '''
+    def __init__(self):
 
-    current_year = dt.date.today().year
-    previous_year = dt.date.today().year - 1
+        self.current_year = dt.date.today().year
+        self.previous_year = dt.date.today().year - 1
