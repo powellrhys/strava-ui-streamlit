5fe8de75183db9ba86527e0ca6237c0088110f95
rhys powell <rhys.powell2@btinternet.com>
2025-07-11 14:26:03 +0100

    apply added to deploy pipeline

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@
 module "frontend" {
     source = "git::https://github.com/powellrhys/powellrhys-iac.git//terraform/azure/app_service?ref=main"
 
-    app_service_name    = "play-cricket-streamlit-frontend"
+    app_service_name    = "strava-streamlit-frontend"
     resource_group_name = var.app_service_resource_group
     location            = var.location
     service_plan_id     = data.azurerm_app_service_plan.app_service_plan.id
